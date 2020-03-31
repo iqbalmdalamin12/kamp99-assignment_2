dd8040db4b2527c0e42d9227783ba12f250e9306
Patrick Kam <kampatrick46@gmail.com>
2020-03-31 19:44:28 -0400

    able to loop

--- main.js
+++ main.js
@@ -6,28 +6,33 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-for(let i = 1; i <= 5; i++){
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', 'images/pic', + i + '.jpeg');
-    thumbBar.appendChild(newImage);
+for(let i = 1; i <= 5; i++) {
 
-    newImage.onclick = function(e){
-    displayedImage.src = e.target.src;  
-    }
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+  thumbBar.appendChild(newImage);
+
+  newImage.onclick = function(e) {
+    displayedImage.src = e.target.src;
+  }
 }
+
 /* Wiring up the Darken/Lighten button */
 
-btn.onclick = function(){
-    const btnClass = btn.getAttribute('thumb-bar');
-    if(btnClass === 'dark'){
+btn.onclick = function() {
 
-        btn.setAttribute('class', 'light');
-        btn.textcontent = 'Lighten';
-        overlay.getElementsByClassName.backgroundColor = 'rgba(0,0,0,0.5)';
-    }
-    else{
-        btn.setAttribute('class', 'dark');
-        btn.textContent = 'Darken';
-        overlay.getElementsByClassName.backgroundColor = 'rgba(0,0,0,0)';
-    }
+  const btnClass = btn.getAttribute('class');
+
+  if(btnClass === 'dark') {
+
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } 
+  else {
+      
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
 }
