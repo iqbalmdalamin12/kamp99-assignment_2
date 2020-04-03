a60a399127b35ee8dae9e1bc28620a7000a664f5
Patrick Kam <kampatrick46@gmail.com>
2020-04-02 21:34:00 -0400

    Revert "final push"
    This reverts commit b353b2819da8059c0a70f21f19308ffe87ab7503.

--- main.js
+++ main.js
@@ -5,42 +5,34 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-//Patrick thought of the for loop to click throught the images
-for(let i = 1; i < 6; i++) {
 
-  //creates new img element
-  const loopImage = document.createElement('img');
-  //adds the components of the image link
-  loopImage.setAttribute('src', 'images/pic' + i + '.jpg');
-  thumbBar.appendChild(loopImage);
+for(let i = 1; i <= 5; i++) {
 
-  //sets image based on the one clicked
-  loopImage.onclick = function(e) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+  thumbBar.appendChild(newImage);
+
+  newImage.onclick = function(e) {
     displayedImage.src = e.target.src;
   }
 }
 
 /* Wiring up the Darken/Lighten button */
-//Md Al Amin Iqbal lighten/darken function
+
 btn.onclick = function() {
 
-  const darkLight = btn.getAttribute('class');
+  const btnClass = btn.getAttribute('class');
 
-  if(darkLight === 'dark') {
+  if(btnClass === 'dark') {
 
-    //change button name to ligjt
     btn.setAttribute('class','light');
     btn.textContent = 'Lighten';
-    //lighten the image
     overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
   } 
-
   else {
 
-    //change button name to darken
     btn.setAttribute('class','dark');
     btn.textContent = 'Darken';
-    //darken the image
     overlay.style.backgroundColor = 'rgba(0,0,0,0)';
   }
 }
